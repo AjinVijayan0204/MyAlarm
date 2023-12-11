2af0e058820535720db95834104106f21daa1dd1
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2023-12-11 15:28:24 +0530

    added stopwatch view

--- MyAlarm/MyAlarm/Stopwatch/StopwatchView.swift
+++ MyAlarm/MyAlarm/Stopwatch/StopwatchView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct StopwatchView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    @ObservedObject var vm: StopwatchViewModel
+        var body: some View {
+            VStack {
+                
+                HStack(spacing: 8) {
+                    Text(vm.hour)
+                    Text(":")
+                    Text(vm.minute)
+                    Text(":")
+                    Text(vm.second)
+                    Text(":")
+                    Text(vm.microSecond)
+                    
+                }
+                .font(.system(size: 25))
+                
+                ButtonView()
+                
+                
+            }
+        }
+        
+        private struct SplitTimesView: View{
+            var body: some View{
+                ScrollView {
+                    Text("Split time")
+                }
+            }
+        }
+        
+        private struct ButtonView: View{
+            var body: some View{
+                HStack {
+                    ForEach(StopwatchViewModel.StopwatchButtons.allCases, id: \.self){ btn in
+                        Button(action: {}) {
+                            Text(btn.getButtonNames())
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 10)
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(btn.buttonColor())
+                    }
+                }
+            }
+        }
 }
 
 struct StopwatchView_Previews: PreviewProvider {
     static var previews: some View {
-        StopwatchView()
+        StopwatchView(vm: StopwatchViewModel())
     }
 }
