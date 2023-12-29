b9fa2e2ca73ce8788f8c6b645954155cdb9fb415
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2023-12-29 09:53:03 +0530

    added current time view

---
+++ MyAlarm/MyAlarm/Clock/CurrentView/CurrentTimeVM.swift
@@ -0,0 +1,29 @@
+//
+//  CurrentTimeVM.swift
+//  MyAlarm
+//
+//  Created by Ajin on 29/12/23.
+//
+
+import Foundation
+
+class CurrentTimeViewModel: ObservableObject{
+    
+    @Published var timeHourMin: String
+    @Published var seconds: String
+    @Published var meridian: String
+        
+    var zone: TimeZone = TimeZone.current
+    var updateTime: Timer{
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.timeHourMin = getTimeFormat(for: .hourMin).string(from: Date())
+            self.seconds = getTimeFormat(for: .seconds).string(from: Date())
+            self.meridian = getTimeFormat(for: .meridian).string(from: Date())
+        }
+    }
+        
+    init() {
+        (self.timeHourMin, self.seconds, self.meridian) = ("", "", "")
+            let _ = updateTime
+    }
+}
---
+++ MyAlarm/MyAlarm/Clock/CurrentView/CurrentTimeView.swift
@@ -0,0 +1,26 @@
+//
+//  CurrentTimeView.swift
+//  MyAlarm
+//
+//  Created by Ajin on 29/12/23.
+//
+
+import SwiftUI
+
+struct CurrentTimeView: View {
+    @ObservedObject var vm = CurrentTimeViewModel()
+    var body: some View {
+        VStack{
+            Text(vm.timeHourMin)
+                .font(.system(size: 50))
+            Text(vm.zone.identifier)
+                .font(.system(size: 20))
+        }
+    }
+}
+
+struct CurrentTimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentTimeView(vm: CurrentTimeViewModel())
+    }
+}
